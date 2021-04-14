f9244c11e7034f27fc1d7c276581bd67945e4079
thanhnv2 <thanhnv2@vnext.com.vn>
2021-04-14 18:48:33 +0700

    done

---
+++ migrations/2_transfer_ownership.js
@@ -0,0 +1,14 @@
+
+// migrations/2_transfer_ownership.js
+const { admin } = require('@openzeppelin/truffle-upgrades');
+ 
+module.exports = async function (deployer, network) {
+  // Use address of your Gnosis Safe
+  const gnosisSafe = '0x1c14600daeca8852BA559CC8EdB1C383B8825906';
+ 
+  // Don't change ProxyAdmin ownership for our test network
+  if (network !== 'test') {
+    // The owner of the ProxyAdmin can upgrade our contracts
+    await admin.transferProxyAdminOwnership(gnosisSafe);
+  }
+};
